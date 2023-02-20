eb6b2f58c99c9fab1a36ee7da8d1293a8a53721c
Donich <Donich440542@mail.ru>
2023-02-20 23:14:24 +0300

    ready+

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"Image-loader/internal/repository"
+	"Image-loader/internal/server"
+	"Image-loader/internal/service"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Привет")
+	var userRepo = repository.NewUserRepository("myNewFile.json")
+	controller := service.NewController(userRepo)
+
+	logger := logrus.New()
+
+	srv := server.NewServer(":8000", logger, controller)
+	srv.RegisterRoutes()
+
+	srv.StartServer()
 }
